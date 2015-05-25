7bb496c8b77405780465e941c3cf3c04c7097e09
Gleb Gladilov <gleb.gladilov@gmail.com>
2015-05-25 13:06:15 +0300

    Reloaded server part

--- BlaBlaMessengerClient/app/src/main/java/cloud/DataReceiverException.java
+++
@@ -1,8 +0,0 @@
-package cloud;
-
-/**
- * Created by VladVin on 11.05.2015.
- */
-public class DataReceiverException extends Exception {
-    public DataReceiverException(String message) {super(message);}
-}
--- BlaBlaMessengerClient/app/src/main/java/data_structures/ConferenceId.java
+++
@@ -1,9 +0,0 @@
-package data_structures;
-
-import java.util.UUID;
-
-public class ConferenceId extends DataObject {
-    public UUID Id;
-
-    public ConferenceId() { Id = UUID.randomUUID(); }
-}
